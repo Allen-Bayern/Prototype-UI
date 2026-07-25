f2a01848bdafbd0f649f604683f09e97f5d1ee17
guangliang2019 <guangliang2018@foxmail.com>
2026-07-25 13:32:37 +0800

    chore(release): prepare 0.2.0-rc.5

--- packages/cli/src/services/proto-style-css.ts
+++ packages/cli/src/services/proto-style-css.ts
@@ -399,6 +399,13 @@ function renderTransformUtility(utility: string): string[] | null {
   if (utility === 'translate-y-0') return ['--pui-translate-y: 0px;', transformValue()];
   if (utility === 'translate-y-px') return ['--pui-translate-y: 1px;', transformValue()];
 
+  const translateMatch = utility.match(/^translate-(x|y)-(.+)$/);
+  if (translateMatch) {
+    const [, axis, rawValue] = translateMatch;
+    const value = spacingValue(rawValue);
+    if (value) return [`--pui-translate-${axis}: ${value};`, transformValue()];
+  }
+
   const scaleMatch = utility.match(/^scale-\[(.+)\]$/);
   if (scaleMatch)
     return [
